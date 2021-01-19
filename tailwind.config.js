d794b70856d66f25ddcc76e9c10925e90f195c6b
Austin Crim <crim.austin@principal.com>
2021-01-19 14:35:02 -0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    './pages/**/*',
+    './components/**/*'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
